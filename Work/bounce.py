4d3518a686a70ab63b863d9d44d1279d0b078478
Nayan Koshiya <33102603+nkoshiya@users.noreply.github.com>
2020-08-28 19:15:59 +0530

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
-#
+## program excercise 1.5 Bouncing Ball
+#bounce.py
+
+wall_height = 100 # meters
+bounce_no = 0
+frac = 3/5
+
+while bounce_no < 10:
+    bounce_no += 1
+    bounce_height = (frac**bounce_no) * wall_height
+    print(f'{bounce_no} {round(bounce_height,4)}')
+    
 # Exercise 1.5
